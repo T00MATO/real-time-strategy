fdce86732a7d10f76329328e67c00b4d1a1d0b89
T00MATO <toomato030517@gmail.com>
2023-05-04 18:56:07 +0900

    PathFinder 추가
    PathFinder는 A* 알고리즘을 기반으로 개발될 예정
    +이전에 개발된 CameraController에 관한 문서를 기재하자

---
+++ Assets/Scripts/RTS/PathFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace RTS
+{
+    public class PathFinder : MonoBehaviour
+    {
+        [SerializeField]
+        private Grid nodeGrid;
+
+        public Vector2[] GetPathPoints(Vector2 start, Vector2 end)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
